3db6a2379ad287b33aa61873ccc360f5bad51cf5
mahmoudplay <66681525+mahmoudplay@users.noreply.github.com>
2026-03-24 17:25:06 +0200

    add backend connection (1.4.3)

--- web/js/script.js
+++ web/js/script.js
@@ -1,30 +1,44 @@
-<<<<<<< HEAD
-function showPopup() {
-    document.getElementById("popup").classList.add("active");
-    document.getElementById("overlay").classList.add("active");
-}
+const api = "http://localhost:3000/"
+const urlLabel = document.querySelector("#shortUrl");
 
-function hidePopup() {
-    document.getElementById("popup").classList.remove("active");
-    document.getElementById("overlay").classList.remove("active");
-}
-=======
-function ValidLink(url) {
+function ValidLink(url){
   const linkRegex =
     /(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])/gim;
   return linkRegex.test(url);
 }
 
-function showPopup() {
-  var input = document.querySelector(".inputUrl input");
+
+async function showPopup() {
+  const input = document.querySelector(".inputUrl input");
+
+
   if (!input.value || !ValidLink(input.value)) {
     document.getElementById("errorPopup").classList.add("active");
     document.getElementById("overlay").classList.add("active");
-    return;
+    
+  }else {
+    
+    const apiReq = await fetch(api, {
+      method: 'POST',
+      headers: {
+      'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+          redirUrl: input.value,
+
+      })
+    });
+
+    const shortedUrl = await apiReq.json();
+
+    urlLabel.innerHTML = shortedUrl.shortedUrl;;
+
+    document.getElementById("popup").classList.add("active");
+    document.getElementById("overlay").classList.add("active");
   }
-  document.getElementById("popup").classList.add("active");
-  document.getElementById("overlay").classList.add("active");
+
 }
+
 
 function hidePopup() {
   document.getElementById("popup").classList.remove("active");
@@ -46,4 +60,13 @@ document.addEventListener("keydown", function (e) {
     hideError();
   }
 });
->>>>>>> 6b68ef6 (add error popout (1.3.3))
+
+function copyShortedUrl() {
+  navigator.clipboard.writeText(urlLabel.innerHTML)
+    .then(() => {
+      alert("Copied ✅");
+    })
+    .catch(err => {
+      console.error("Error:", err);
+    });
+}
